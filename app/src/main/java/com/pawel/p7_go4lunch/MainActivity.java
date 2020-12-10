5e8c2c497492b85548802e7625fa078ff0baebea
Pamento <pamento.dev@gmail.com>
2020-12-10 11:57:06 +0100

    initial commit

---
+++ app/src/main/java/com/pawel/p7_go4lunch/MainActivity.java
@@ -0,0 +1,36 @@
+package com.pawel.p7_go4lunch;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.pawel.p7_go4lunch.databinding.ActivityMainBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        com.pawel.p7_go4lunch.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view =binding.getRoot();
+        setContentView(view);// if this will be working with the fragments ?
+
+        //BottomNavigationView navView = findViewById(R.id.nav_view);
+        //BottomNavigationView nevView = bi
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
