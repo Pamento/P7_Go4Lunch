b143e3f11dfcbd8036917dc1e599c863bc958f8f
Pamento <pamento.dev@gmail.com>
2020-12-10 14:47:57 +0100

    add firebase to applicaiton Go4Lunch

--- app/src/main/java/com/pawel/p7_go4lunch/MainActivity.java
+++ app/src/main/java/com/pawel/p7_go4lunch/MainActivity.java
@@ -19,10 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         com.pawel.p7_go4lunch.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view =binding.getRoot();
-        setContentView(view);// if this will be working with the fragments ?
+        setContentView(view);
 
-        //BottomNavigationView navView = findViewById(R.id.nav_view);
-        //BottomNavigationView nevView = bi
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
