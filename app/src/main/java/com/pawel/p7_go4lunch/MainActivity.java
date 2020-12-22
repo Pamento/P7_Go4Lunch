1287530a9bbaad857160768495948b3436ae5a86
Pamento <pamento.dev@gmail.com>
2020-12-22 10:58:52 +0100

    correct bug on toolbar search

--- app/src/main/java/com/pawel/p7_go4lunch/MainActivity.java
+++ app/src/main/java/com/pawel/p7_go4lunch/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthMethodPickerLayout;
@@ -21,6 +20,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
